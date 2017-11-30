5bda01dd34f83056bd913f1bc55e9f01a594a405
Tony Boyles <nsp3@cdc.gov>
2017-11-30 15:31:22 -0500

    Revised to be more general
    No longer necessarily need be run on merely LW2 content.

--- build.js
+++ build.js
@@ -4,7 +4,6 @@ const cheerio = require('cheerio');
 const makepub = require('nodepub');
 const jetpack = require('fs-jetpack');
 const { execSync } = require('child_process');
-const baseurl = 'https://www.lesserwrong.com';
 
 var version = process.argv.length > 2 ? process.argv[2] : 'default';
 
@@ -20,8 +19,8 @@ var base_content = jetpack.read('template.xhtml');
 
 function addChapterToBook(html, path){
   let $ = cheerio.load(html);
-  let title = $('div.posts-page-content-header-title').text();
-  if(title == ''){
+  let title = $(config.titleSelector).text();
+  if(title === ''){
     console.log('Server barfed on', path);
     jetpack.remove('cache/' + path.trim().split('/').pop() + '.html');
     // Normally, a script would submit another request to download the missing file.
@@ -30,7 +29,7 @@ function addChapterToBook(html, path){
   let safe_title = title.toLowerCase().replace(/ /g, '-');
   let newDoc = cheerio.load(base_content);
   newDoc('body').append('<div id="'+safe_title+'"></div>');
-  newDoc('div').append('<h1>'+title+'</h1>').append($('div.posts-page-content-body-html').html());
+  newDoc('div').append('<h1>'+title+'</h1>').append($(config.contentSelector).html());
   epub.addSection(title, newDoc('body').html());
 }
 
@@ -39,8 +38,8 @@ config.urls.forEach(path => {
   if(jetpack.exists(cache_path)){
     var html = jetpack.read(cache_path);
   } else {
-    console.log('Scraping', baseurl + path);
-    var r = execSync('curl ' + baseurl + path);
+    console.log('Scraping', config.metadata.source + path);
+    var r = execSync('curl ' + config.metadata.source + path);
     var html = r.toString();
     jetpack.write(cache_path, html);
   }
